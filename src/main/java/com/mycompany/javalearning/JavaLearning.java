b0c7c7dad7d922566d4013a8015c82c4a24e971b
Filastir <salifou.tiroudas@gmail.com>
2024-04-29 13:01:27 +0000

    Ajout d'une fonction de calcul de la superficie d'un rectangle

--- src/main/java/com/mycompany/javalearning/JavaLearning.java
+++ src/main/java/com/mycompany/javalearning/JavaLearning.java
@@ -13,4 +13,14 @@ public class JavaLearning {
     public static void main(String[] args) {
         System.out.println("I'm learning Java with funny guys from SONABEL!");
     }
+    
+    /**
+     * Methode pour calculer la supercie d'un rectangle
+     * @param longueur
+     * @param largeur
+     * @return 
+     */
+    public float calculerSperficieRectangle(final float longueur,final float largeur){
+        return longueur * largeur;
+    }
 }
